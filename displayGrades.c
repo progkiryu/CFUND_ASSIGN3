f8a37795afb2146219a7ef918e9c0e6f47f9077f
Remi <andrewlu360@gmail.com>
2025-05-09 08:50:25 +1000

    Drafting displayGrades

--- displayGrades.c
+++ displayGrades.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+//Work in progress
+/*
+void displayGrades(node** inputNode){
+    char student_name[MAX_NAME_LEN];
+    student current_student;
+    int found;
 
-void displayGrades();
+    printf("Please input student name: ");
+    fgets(student_name, sizeof(student_name), stdin);
+    flush(student_name, MAX_NAME_LEN);
+
+    found = searchStudent(student_name, &current_student, *inputNode);
+    if(found != 0){
+        //false
+        printf("This student does not exist");
+    }
+    else if(found == 0){
+        //true
+        current_student
+    }                      */
