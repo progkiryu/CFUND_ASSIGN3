a707438ebe418472828148da1f2455ce060053bc
Remi <andrewlu360@gmail.com>
2025-05-09 09:41:07 +1000

    Finished displayGrades draft

--- displayGrades.c
+++ displayGrades.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 //Work in progress
-/*
+
 void displayGrades(node** inputNode){
     char student_name[MAX_NAME_LEN];
     student current_student;
@@ -13,11 +13,27 @@ void displayGrades(node** inputNode){
     flush(student_name, MAX_NAME_LEN);
 
     found = searchStudent(student_name, &current_student, *inputNode);
+    
     if(found != 0){
         //false
         printf("This student does not exist");
     }
+    
     else if(found == 0){
         //true
-        current_student
-    }                      */
+        int idx, grade;
+        int subjectLen = student_name.subjectLen;
+        char subject[MAX_SUB_LEN];
+        
+        printf("%-*s %s\n", MAX_SUB_LEN, Subject, Grades);
+        printf("----------------------------------\n");
+
+        for(idx = 0; idx < subjectLen; idx++){
+        grade = current_student.subjects[idx].mark;
+        subject = current_student.subjects[idx].name;
+        
+        printf("%-*s", MAX_SUB_LEN, subject);
+        printf(" %.2d\n", grade);
+        }
+    }
+}
