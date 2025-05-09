8710f3718838820394fe69c9063c627b3bc191c3
Remi <andrewlu360@gmail.com>
2025-05-09 13:17:50 +1000

    Edit comments to suit ANSI

--- Remove Student File.c
+++ Remove Student File.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 void removeStudent(node** inputNode, int* studentLen);
-//still drafting, W.I.P
+/*still drafting, W.I.P*/
 
 void removeStudent(node** inputNode, int* studentLen){
 
@@ -16,12 +16,12 @@ void removeStudent(node** inputNode, int* studentLen){
 
     found = searchStudent(student_name, &currentStudent, *inputNode);
     if(found != 0){
-        //false
+        /*false*/
         printf("This student does not exist");
     }
 
     else if(found == 0){
-        //true
+        /*true*/
         node* temp,* prev_node,* later_node, * removeStudent_node;
         temp = *inputNode;
 
@@ -30,12 +30,12 @@ void removeStudent(node** inputNode, int* studentLen){
             removeStudent_node = temp -> next;
             later_node = temp -> next -> next;
             
-            //if student name lines up with node in linked list 
+            /*if student name lines up with node in linked list*/ 
             if(strcmp(removeStudent_node -> nodeStudent.name, currentStudent.name) == 0){
                 break;
             }
 
-            //considering condition: entry node is the student to be removed
+            /*considering condition: entry node is the student to be removed*/
             else if(strcmp(prev_node -> nodeStudent.name, currentStudent.name) == 0){
                 removeStudent_node = prev_node;
                 later_node = removeStudent_node -> next;
@@ -45,15 +45,15 @@ void removeStudent(node** inputNode, int* studentLen){
             temp = temp -> next;
         }
         
-        //mending the linked list, removing student from list
+        /*mending the linked list, removing student from list*/
         if(prev_node != NULL){
             prev_node -> next = later_node;
         }
-        //if entry node was to be removed, changes the linked list head
+        /*if entry node was to be removed, changes the linked list head*/
         else if(prev_node == NULL){
             *inputNode = later_node;
         }
-        //free linked list memory
+        /*free linked list memory*/
         free(removeStudent_node);
 
         *studentLen -= 1;
@@ -61,7 +61,7 @@ void removeStudent(node** inputNode, int* studentLen){
     }
 }
 
-        //finding the nodes before and after (done)
-        //Remove student from linked list (done)
-        //check to see if prevNode or laterNode is NULL (done)
-        //join ends of linked list together (done)
+        /*finding the nodes before and after (done)
+        Remove student from linked list (done)
+        check to see if prevNode or laterNode is NULL (done)
+        join ends of linked list together (done) */
