b64eefc533f9c01c10e2e7d4bc003e1a1a723f34
Remi <andrewlu360@gmail.com>
2025-05-18 17:01:21 +1000

    Added decryption draft

--- Encryption-Decryption functions.c
+++ Encryption-Decryption functions.c
@@ -67,3 +67,51 @@ void encryptFile(){
         fclose(encryptFile);
     }
 }
+
+
+void decryptFile(){
+    int i;
+    char key[10];
+    char encryptedfilename[256];
+    char filename[256], student_name[MAX_NAME_LEN];
+    
+    printf("Enter student name to decrypt: ");
+    fgets(student_name, MAX_NAME_LEN, stdin);
+    flush(student_name, MAX_NAME_LEN);
+
+    strcpy(encryptedfilename, "encrypted");
+    strcat(encryptedfilename, student_name);
+    strcat(encryptedfilename, ".txt");
+
+    /*open file to check if it exists*/
+    FILE *encryptedFile = fopen(encryptedfilename, "r");
+    
+    /*stops function if non-existent*/
+    if(encryptedFile == NULL){
+        printf("File does not exist\n");
+        return;
+    }
+
+    printf("Please enter the password: ");
+    fgets(key, 10, stdin);
+    flush(key, 10);
+    
+    /*naming compressed student file*/
+    strcpy(filename, "grades_compressed/");
+    strcat(filename, "test")
+    strcat(filename, student_name);
+    strcat(filename, ".txt");
+
+    FILE *dataFile = fopen(filename, "w");
+    
+    unsigned char byte;
+    
+    /*decrypting each character*/
+    for(i=0; fscanf(encryptedFile, "%2hhX", &byte) == 1; i++){
+            char decrypted = byte ^ key[i % strlen(key)];
+            fprintf(dataFile, "%c", decrypted);
+    }
+
+    fclose(encryptedFile);
+    fclose(dataFile);
+}
