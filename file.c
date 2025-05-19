a7a37baf87069db25480520b8712709cd81a7c84
progkiryu <denmesa24@outlook.com>
2025-05-19 16:23:12 +1000

    file formatting

--- file.c
+++ file.c
@@ -25,7 +25,7 @@ void saveToFile(node* head) {
     if (check != NULL) {
         fclose(check);
         printf("Warning: file \"%s\" already exists. Overwrite? (Y/N): ", filename);
-
+    }
     while (getchar() != '\n'); 
 
     char choice;
@@ -33,45 +33,45 @@ void saveToFile(node* head) {
 
     if (choice != 'Y' && choice != 'y') {
         printf("Operation cancelled.\n");
-    return;
+        return;
     }
     
-FILE* fp = fopen(filename, "w");
+    FILE* fp = fopen(filename, "w");
     if (fp == NULL) {
         printf("Error: Cannot open file for writing.\n");
         return;
     }
 
-/*Store the student information*/
+    /*Store the student information*/
     node* curr = head;
     while (curr != NULL) {
         fprintf(fp, "Student: %s\n", curr->nodeStudent.name);
         int i;
-    while (i < 5) {
-        fprintf(fp, "- %s: %d marks | ", 
-                curr->nodeStudent.subjects[i].name, 
-                curr->nodeStudent.subjects[i].mark);
+        while (i < 5) {
+            fprintf(fp, "- %s: %d marks | ", 
+                    curr->nodeStudent.subjects[i].name, 
+                    curr->nodeStudent.subjects[i].mark);
 
-        if (strlen(curr->nodeStudent.subjects[i].comment) == 0) {
-            fprintf(fp, "No comment");
-        } else {
-            char buf[100];
-            strncpy(buf, curr->nodeStudent.subjects[i].comment, 99);
-            buf[99] = '\0';
-            /*for (int j = 0; buf[j]; j++) {
-                if (buf[j] == '\n') buf[j] = ' ';
-            }*/
-            fprintf(fp, "Comment: %s", buf);
+            if (strlen(curr->nodeStudent.subjects[i].comment) == 0) {
+                fprintf(fp, "No comment");
+            } else {
+                char buf[100];
+                strncpy(buf, curr->nodeStudent.subjects[i].comment, 99);
+                buf[99] = '\0';
+                /*for (int j = 0; buf[j]; j++) {
+                    if (buf[j] == '\n') buf[j] = ' ';
+                }*/
+                fprintf(fp, "Comment: %s", buf);
+            }
+            fprintf(fp, "\n");
+            i++;
         }
         fprintf(fp, "\n");
-        i++;
+        curr = curr->next;
     }
 
-    fprintf(fp, "\n");
-    curr = curr->next;
-    }
 
     fclose(fp);
     printf("Successfully saved to: %s\n", filename);
 } 
-}
+
